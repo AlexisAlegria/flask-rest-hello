c9a4561c42151d09b614ae9926959dac7e922b87
AlexisAlegria <alexisalegriav@gmail.com>
2021-11-10 01:34:48 +0000

    setting GET,POST,PUT,DELETE methods

--- src/main.py
+++ src/main.py
@@ -32,12 +32,55 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body_user = request.get_json()
+    new_user = User(email=request_body_user["email"], password=request_body_user["password"])
+    db.session.add(new_user)
+    db.session.commit()
 
-    return jsonify(response_body), 200
+    return jsonify(request_body_user), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def edit_user(user_id):
+    request_body_user = request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+
+    if "email" in request_body_user:
+        user1.email = request_body_user["request_body_user"]
+
+    db.session.commit()
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return jsonify("User deleted"), 200
+
+@app.route('/cart', methods=['DELETE'])
+def add_to_cart():
+    request_body_user = request.get_json()
+    product_id = request_body_user["product_id"]
+    user_id = request_body_user["user_id"]
+
+    record = ShoppingCart.query.filter_by(user_id=user_id, product_id=product_id)
+    if record is None:
+        record = ShoppingCart(user_id=user_id, product_id=product_id, quantity=request_body_user["quantity"])
+        db.session.add(new_user)
+    else:
+        record.quantity = request_body_user["quantity"]
+
+    db.session.commit()
+    return jsonify(record), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
